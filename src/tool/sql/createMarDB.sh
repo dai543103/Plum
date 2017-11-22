b605e4b540e2e1c8ad19fcb9635f66e3a867daa1
dengjiatai <529745473@qq.com>
2017-11-22 16:13:41 +0800

    删除无用代码

--- src/tool/sql/createMarDB.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# != 1 ]; then 
-	echo "please input db name" 
-	exit 1;
-fi
-dbname=$1
-mysql -uroot -p123456 -hlocalhost -e "CREATE DATABASE IF NOT EXISTS $dbname DEFAULT CHARSET utf8 COLLATE utf8_general_ci"
-mysql -uroot -p123456 -hlocalhost -D $dbname < ./dmr.sql
-mysql -uroot -p123456 -hlocalhost -D $dbname < ./update.sql
